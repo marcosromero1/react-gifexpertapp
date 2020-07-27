d6a630531ff7a4ce87810f5698c62b420a33577a
MarcosRomero <marcosromero878@gmail.com>
2020-07-27 20:51:55 -0300

    docs agregado

---
+++ docs/precache-manifest.4a9ff261b9f140b22c5593b52b30389f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65503d3771c879c534eea69056ec7769",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8c55f33b299a95720d2",
+    "url": "/static/css/main.bf4eea85.chunk.css"
+  },
+  {
+    "revision": "3ec50c8fead662706b17",
+    "url": "/static/js/2.637c77a8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.637c77a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8c55f33b299a95720d2",
+    "url": "/static/js/main.ca53e75b.chunk.js"
+  },
+  {
+    "revision": "2beaf3c9299542d4240e",
+    "url": "/static/js/runtime-main.c8ba4116.js"
+  }
+]);
